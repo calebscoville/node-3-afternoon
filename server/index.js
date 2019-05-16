d0f91831f4154ca09635d493da65801ec04f7329
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-16 17:20:19 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
+const products_controller = require("./products_controller")
 
 const app = express()
 
 const {SERVER_PORT, CONNECTION_STRING} = process.env
 
-massive(CONNECTION_STRING).then((dbInstance) => {
-    app.set('db', dbInstance)
-    console.log('database set')
-    app.set("db", dbInstance)
+massive(CONNECTION_STRING)
+.then((dbInstance) => {
+    app.set('db', dbInstance);
+})
+  .catch(err => console.log(err)) 
     
-app.use(express.json())    
+app.use(express.json())  
+
+app.post('/api/products', products_controller.create);
+app.get('/api/products')
+
     
     app.listen(process.env.SERVER_PORT, () => console.log(`You are very blessed my son on ${SERVER_PORT}`))
-})
