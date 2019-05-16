1bab2682096b402c586b059cc7d3356a26235f6b
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-16 16:09:10 -0600

    fifth

--- server/products_controller.js
+++ server/products_controller.js
@@ -25,6 +25,32 @@ module.exports = {
         const dbInstance = req.app.get('db');
 
         dbInstance.read.products()
-        .then
+        .then ( products => res.status(200).send( products ) )
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oopsadaisies! Somethin' went awry. Our engineers are now working on it this very minute."});
+            console.log(err)
+        });
+    },
+
+    update: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+
+        dbInstance.update_product()
+        .then( () => res.sendStatus(200) )
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oopsadaisies! Somethin' went awry. Our engineers are now working on it this very minute."})
+            console.log(err)
+        });
+    },
+
+    delete: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+
+        dbInstance.delete_product()
+        .then( () => res.sendStatus(200) )
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oopsadaisies! Somethin' went awry. Our engineers are now working on it this very minute."})
+            console.log(err)
+        });
     }
-}
+};
